85476cb95f7ec5e05ee3b52f3907d214af70b082
Durjoy Barua <durjoybarua8115@example.com>
2024-01-17 19:51:48 +0600

    push

--- Data Structures/stack_linkedList.cpp
+++ Data Structures/stack_linkedList.cpp
@@ -4,7 +4,66 @@ struct node{
     int data;
     node* next;
 };
-node* root=NULL;
+node *root=NULL, *tail=NULL;
 void insert(int data){
+    node* current_node= root;
+    if(root==NULL){
+        root= new node();
+        root->data= data;
+        root->next= NULL;
+        tail= root;
+    }
+    else{
+        node* new_node= new node();
+        new_node->data= data;
+        new_node->next=NULL;
+        tail->next= new_node;
+        tail=tail->next;
+    }
+}
+void deletion() {
+    node *current = root;
+    node *pre = NULL;
+
+    if (current == NULL) {
+        cout << "Stack underflow\n";
+        return;
+    }
     
+    while (current->next != NULL) {
+        pre = current;
+        current = current->next;
+    }
+
+    if (root==pre) {
+        delete root;
+        root = tail = NULL;
+    } else {
+        delete pre->next;
+        pre->next = NULL;
+        tail = pre;
+    }
+}
+
+void print(){
+    node* temp= root;
+    while(temp!=NULL){
+        cout<<temp->data<<endl;
+        temp= temp->next;
+    }
+    delete(temp);
+}
+
+int main(){
+    insert(25);
+    insert(26);
+    insert(27);
+    insert(28);
+    insert(29);
+    insert(30);
+    print();
+    cout<<"After deleting:\n";
+    deletion();
+    print();
+
 }
