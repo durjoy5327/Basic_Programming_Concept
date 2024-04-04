b58447c3606e69b8ef50f458361523c80b761fbd
Durjoy Barua <durjoybarua8115@example.com>
2024-04-04 20:04:22 +0600

    push

--- Data Structures using class/linked_list.cpp
+++ Data Structures using class/linked_list.cpp
@@ -45,8 +45,87 @@ void print(){
         cout<<temp->data<<" ";
         temp=temp->next;
     }
-
 }
+
+//this delete function is use for delete last element
+void top(){
+        if(head==NULL){
+            cout<<"Linked List is empty\n";
+            return;
+        }
+        Node* temp= head;
+        Node* secondLast=NULL;
+        while(temp->next!=NULL){
+            secondLast= temp;
+            temp= temp->next;
+        }
+
+        if(secondLast== NULL){
+        head = NULL;
+    } else {
+        secondLast->next = NULL;
+    }
+    delete temp;
+    }
+
+//this function is to delete a specific element
+    void deleted(int element) {
+    if (head == NULL) {
+        cout << "Linked List is empty\n";
+        return;
+    }
+
+    if (head->data == element) {
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return;
+}
+
+    Node* temp = head;
+    Node* prev = NULL;
+
+    while (temp != NULL && temp->data != element) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        cout << "Element not found\n";
+        return;
+    }
+    // If the element is not the last one
+    if (temp->next != NULL) {
+        prev->next = temp->next;
+    } else {
+        // If the element is the last one
+        prev->next = NULL;
+    }
+
+    delete temp;
+}
+
+void reverse(){
+    if(head==NULL ){
+        cout<<"Linked List is empty\n";
+    }
+    Node* prev = NULL;
+    Node* current = head;
+    Node* Next = NULL;
+
+    while (current != NULL) {
+        Next = current->next; // Store next node
+        current->next = prev; // Reverse current node's pointer
+        prev = current; // Move pointers one position ahead of the previous node
+        current = Next;  // moving forward the current node
+    }
+
+    head = prev;
+    
+}
+
+
+
 };
 
 int main(){
@@ -56,6 +135,12 @@ int main(){
     list.insert(50);
     list.insert(26);
     list.print();
-
+    cout<<endl;
+    list.deleted(25);
+    list.print();
+    cout<<endl;
+    list.reverse();
+    list.print();
+    cout<<endl;
     return 0;
 }
---
+++ Data Structures using class/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+    list.deleted(25);
